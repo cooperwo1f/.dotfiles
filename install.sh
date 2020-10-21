bfeea2d16a64f291d5f0e5e7363880cf29927c9e
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-10-22 03:10:08 +1030

    added option for clean install

--- install.sh
+++ install.sh
@@ -9,11 +9,26 @@ echo "configdir = $CONFIGDIR"
 
 linkFiles=`find $BASEDIR/* -maxdepth 0 -type d -exec realpath --relative-to=$BASEDIR {} \;`
 
+
+if [ $1 = 'clean' ]; then
+  echo "doing clean install"
+fi
+
 mkdir -p $CONFIGDIR
 
 for i in $linkFiles; do
   mv -i -v $BASEDIR/$i $CONFIGDIR/
+
+  if [ $1 = 'clean' ]; then
+    rm -r -f $CONFIGDIR/$i
+  fi
+
 done
+
+if [ $1 = 'clean' ]; then
+  rm -r -f $HOME/.bashrc
+  rm -r -f $HOME/.vim
+fi
 
 echo "linking .bashrc"
 ln -s -v -i $CONFIGDIR/bash/bashrc $HOME/.bashrc
