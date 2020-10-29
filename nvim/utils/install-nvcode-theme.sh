a985bc65ddbaf9b8c8afce4512c6aaa84074aa39
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-10-29 19:06:28 +1030

    implemented nvcode color generator into install script

--- nvim/utils/install-nvcode-theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp nvcode.yml ../autoload/plugged/nvcode.vim/nvcode.yml &&
-cd ../autoload/plugged/nvcode.vim/ &&
-sh update_colorscheme.sh
--- nvim/utils/install.sh
+++ nvim/utils/install.sh
@@ -153,4 +153,7 @@ which nvim > /dev/null && installplugins
 
 installcocextensions
 
+$HOME/.config/nvim/autoload/plugged/nvcode.vim/generate $HOME/.config/nvim/utils/nvcode.yml > $HOME/.config/nvim/autoload/plugged/nvcode.vim/colors/nvcode.vim
+
+
 echo "install and activate a font from here: https://github.com/ryanoasis/nerd-fonts"
