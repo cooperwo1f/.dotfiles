e74e769be1eb2471f2fe598d03128782285af39b
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-11-09 01:33:52 +1030

    removed rsnapshot

--- rsnapshot/utils/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "installing rsnapshot"
-sudo pacman -S --needed rsnapshot
-echo "updating config"
-sudo sh -c "sed "23s/.#HOSTNAME#/$HOSTNAME/" $HOME/.config/rsnapshot/utils/rsnapshot.conf > /etc/rsnapshot.conf"
