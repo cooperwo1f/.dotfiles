4efaeadbae6ccbc2047421bba10596e12b2326aa
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-11-25 08:25:22 +1030

    added background as symbolic link

--- i3/utils/install.sh
+++ i3/utils/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo mv $HOME/.config/i3/utils/plasma-i3.sh /usr/local/bin/
+sudo chmod +x /usr/local/bin/plasma-i3.sh
 sudo mv $HOME/.config/i3/utils/plasma-i3.desktop /usr/share/xsessions/
