d2282d12507937890c5f00acbd6c83766db19f30
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-11-25 08:42:29 +1030

    updated install file to install i3-gaps

--- i3/utils/install.sh
+++ i3/utils/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+sudo chmod +x $HOME/.config/i3/utils/plasma-i3.sh
 sudo cp -i $HOME/.config/i3/utils/plasma-i3.sh /usr/local/bin/
-sudo chmod +x /usr/local/bin/plasma-i3.sh
 sudo cp -i $HOME/.config/i3/utils/plasma-i3.desktop /usr/share/xsessions/
-sudo pacman -S --needed feh picom
+sudo pacman -S --needed feh picom i3-gaps
