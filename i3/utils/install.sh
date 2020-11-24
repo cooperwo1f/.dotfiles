54516816acf9b09b16a8402d914557eb58f8e69b
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-11-25 08:39:19 +1030

    updated i3 install to not move files

--- i3/utils/install.sh
+++ i3/utils/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo mv $HOME/.config/i3/utils/plasma-i3.sh /usr/local/bin/
+sudo cp -i $HOME/.config/i3/utils/plasma-i3.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/plasma-i3.sh
-sudo mv $HOME/.config/i3/utils/plasma-i3.desktop /usr/share/xsessions/
+sudo cp -i $HOME/.config/i3/utils/plasma-i3.desktop /usr/share/xsessions/
+sudo pacman -S --needed feh picom
