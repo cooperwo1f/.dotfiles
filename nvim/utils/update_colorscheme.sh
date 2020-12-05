a5e408eebef4249be03ef1d3627608771d170a18
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2020-12-06 03:59:47 +1030

    updated nvim config with new nvcode repo

--- nvim/utils/update_colorscheme.sh
+++ nvim/utils/update_colorscheme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$HOME/.config/nvim/utils/generate $HOME/.config/nvim/utils/nvcode.yml > $HOME/.config/nvim/autoload/plugged/nvcode.vim/colors/nvcode.vim
+$HOME/.config/nvim/utils/generate $HOME/.config/nvim/utils/nvcode.yml > $HOME/.config/nvim/autoload/plugged/nvcode-color-schemes.vim/colors/nvcode.vim
