80e2768d3817e6fe4cbd06aaf06624d8c8659061
cooperwo1f <43880675+cooperwo1f@users.noreply.github.com>
2024-04-29 16:55:43 +1000

    made variable names nicer and better

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONFIGDIR="$HOME/.config"
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG_DIR="$HOME/.config"
 
 sudo echo "got root"
 
 echo "homepath = $HOME"
-echo "basedir = $BASEDIR"
-echo "configdir = $CONFIGDIR"
+echo "BASE_DIR = $BASE_DIR"
+echo "CONFIG_DIR = $CONFIG_DIR"
 
-linkFolders=`find $BASEDIR/* -maxdepth 0 -type d -exec realpath --relative-to=$BASEDIR {} \;`
-linkFiles=`find $BASEDIR/. -maxdepth 1 -type f -name ".[^.]*" -exec realpath --relative-to=$BASEDIR {} \;`
+linkFolders=`find $BASE_DIR/* -maxdepth 0 -type d -exec realpath --relative-to=$BASE_DIR {} \;`
+linkFiles=`find $BASE_DIR/. -maxdepth 1 -type f -name ".[^.]*" -exec realpath --relative-to=$BASE_DIR {} \;`
 
 if [ "$1" = "clean" ]; then
   echo "doing clean install"
 fi
 
-mkdir -p $CONFIGDIR
+mkdir -p $CONFIG_DIR
 
 echo "linking folders"
 for i in $linkFolders; do
   if [ "$1" = "clean" ]; then
-    rm -r -f $CONFIGDIR/$i
+    rm -r -f $CONFIG_DIR/$i
   fi
   
-  ln -s -v -i $BASEDIR/$i $CONFIGDIR/
+  ln -s -v -i $BASE_DIR/$i $CONFIG_DIR/
 
 done
 
@@ -38,17 +38,17 @@ for i in $linkFiles; do
     rm -r -f $HOME/$i
   fi
   
-  cp -v -i $BASEDIR/$i $HOME/
+  cp -v -i $BASE_DIR/$i $HOME/
 
 done
 
 if [ "$1" = "clean" ]; then
-  sh $CONFIGDIR/nvim/utils/install.sh
-  sh $CONFIGDIR/i3/utils/install.sh
+  sh $CONFIG_DIR/nvim/utils/install.sh
+  sh $CONFIG_DIR/i3/utils/install.sh
 fi
 
-git update-index --skip-worktree $BASEDIR/kicad/eeschema
-git update-index --skip-worktree $BASEDIR/kicad/pcbnew
+git update-index --skip-worktree $BASE_DIR/kicad/eeschema
+git update-index --skip-worktree $BASE_DIR/kicad/pcbnew
 
 echo "install background using this command"
 echo "cd i3; ln -s /path/to/background background"
